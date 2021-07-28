6899e3fce6ccb19e73a037f0eba77757dd627741
Your Name <you@example.com>
2021-07-28 23:48:17 +0530

    first problem

---
+++ problem1/src/eg1/Website.java
@@ -0,0 +1,16 @@
+package eg1;
+
+public class Website {
+
+	public static void main(String[] args) {
+		String Domain_name ="https://en.wikipedia.org/wiki/Main_Page";
+		System.out.println("Domain_name.substring(8,24) : " + Domain_name.substring(8,24));
+
+	}
+	
+	
+
+
+
+
+}
